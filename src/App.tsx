dbe402a36a07a451f52ad6b8172dc488af7404ee
marcjulianschwarz <info@marc-julian.de>
2023-01-10 20:05:28 +0100

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import TodoList from "./SimpleTodo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [todoLists, setTodoLists] = useState<string[]>([]);
+
+  const firstInputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    firstInputRef.current?.focus();
+  });
+
+  if (todoLists.length > 0) {
+    return (
+      <div
+        className="todo-lists-container"
+        onKeyDownCapture={(event: React.KeyboardEvent<HTMLDivElement>) => {
+          if (event.metaKey && event.shiftKey && event.key == "p") {
+            event.preventDefault();
+            event.stopPropagation();
+            setTodoLists([...todoLists, prompt("Enter a new todo list name") || "Default"]);
+          }
+        }}
+      >
+        {todoLists.map((title, idx) => {
+          return <TodoList title={title} key={idx} />;
+        })}
+      </div>
+    );
+  } else {
+    return (
+      <div className="first-note-list-creation-container">
+        <input
+          type="text"
+          ref={firstInputRef}
+          placeholder="First Todo List"
+          onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => {
+            if (event.key == "Enter") {
+              setTodoLists([...todoLists, event.currentTarget.value]);
+              event.currentTarget.value = "";
+            }
+          }}
+        ></input>
+      </div>
+    );
+  }
 }
-
-export default App;
